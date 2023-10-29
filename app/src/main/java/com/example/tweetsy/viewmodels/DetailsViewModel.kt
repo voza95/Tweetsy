2b3324032b08b4fb8699aa33fe8dcd6a4885a4c7
Vza <35027648+voza95@users.noreply.github.com>
2023-10-29 22:19:42 +0530

    Fixed detail screen crash

--- app/src/main/java/com/example/tweetsy/viewmodels/DetailsViewModel.kt
+++ app/src/main/java/com/example/tweetsy/viewmodels/DetailsViewModel.kt
@@ -5,10 +5,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.tweetsy.models.TweetListItem
 import com.example.tweetsy.repository.TweetRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class DetailsViewModel @Inject constructor(
     private val repository: TweetRepository,
     private val savedStateHandle: SavedStateHandle
