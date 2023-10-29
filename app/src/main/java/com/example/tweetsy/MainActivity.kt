a365036fd6b5d7102ce5f59e5022be3c8b5554c0
Vza <35027648+voza95@users.noreply.github.com>
2023-10-29 22:07:42 +0530

    Update UI

--- app/src/main/java/com/example/tweetsy/MainActivity.kt
+++ app/src/main/java/com/example/tweetsy/MainActivity.kt
@@ -3,7 +3,14 @@ package com.example.tweetsy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -20,12 +27,25 @@ class MainActivity : ComponentActivity() {
 
     lateinit var tweetsyApi: TweetsyApi
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             TweetsyTheme {
-                App()
+                Scaffold(
+                    topBar = {
+                        TopAppBar(title = {
+                            Text(text = "Tweetsy")
+                        })
+                    }
+                ) {
+                    Box(
+                        modifier = Modifier.padding(it)
+                    ) {
+                        App()
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/example/tweetsy/screens/CategoryScreen.kt
+++ app/src/main/java/com/example/tweetsy/screens/CategoryScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -35,13 +36,21 @@ fun CategoryScreen(onClick: (category: String)->Unit) {
     val categoryViewModel: CategoryViewModel = hiltViewModel()
     val categories: State<List<String>> = categoryViewModel.categories.collectAsState()
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(8.dp),
-        verticalArrangement = Arrangement.SpaceAround
-    ) {
-        items(categories.value.distinct()) {
-            CategoryItem(category = it, onClick = onClick)
+    if(categories.value.isEmpty()) {
+        Box(
+            modifier = Modifier.fillMaxSize(1f),
+            contentAlignment = Alignment.Center) {
+            Text(text = "Loading...", style = MaterialTheme.typography.bodyMedium)
+        }
+    } else {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(8.dp),
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            items(categories.value.distinct()) {
+                CategoryItem(category = it, onClick = onClick)
+            }
         }
     }
 }
